dcb7540d46f3fbd57a30cf82f9befd182e1cab36
3nylar <adefilaadebukola39@gmail.com>
2026-04-22 11:07:41 +0100

    Made changes to vscode settings.json and tsconfig.jscon

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,46 @@
+import { Tabs } from "expo-router";
+import { tabs } from "@/constants/data";
+import { View } from "react-native";
+import { clsx }  from "clsx";
+import { Image } from "expo-image";
+
+const TabLayout = () => {
+    const TabIcon = ({ focused, icon }: TabIconProps) => {
+        return (
+            <View className="tabs-icon">
+                <View className={clsx('tabs-pill', focused && 'tabs-active')}>
+                    <Image 
+                        source={icon} 
+                        className="tabs-glyph"
+                    />
+                </View>
+            </View>
+        )
+    };
+
+    return (
+        <Tabs 
+            screenOptions={{ 
+                headerShown: false,
+                tabBarShowLabel: false,
+            }}
+        >
+            {tabs.map(( tab ) => (
+                <Tabs.Screen 
+                    key={tab.name} 
+                    name={tab.name} 
+                    options={{ 
+                        title: tab.title, 
+                        tabBarIcon: ({ focused }) => (
+                            <TabIcon focused={focused} icon={tab.icon} />
+                        )
+                             
+                    }}
+                />
+            ))}
+        </Tabs>
+    )
+}
+    
+
+export default TabLayout;
--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,15 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
-module.exports = defineConfig([
-  expoConfig,
+export default defineConfig([
   {
-    ignores: ['dist/*'],
-  },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: "./tsconfig.json"
+        }
+      }
+    }
+  }
 ]);
